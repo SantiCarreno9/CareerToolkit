ce93fe92f4a87f24c3cbb3f6f64d83df900298ae
Santiago Carreno <91355710+SantiCarreno9@users.noreply.github.com>
2025-08-18 00:10:02 -0400

    Frontend: Delete section error fixed (condition for profile entries section)

--- frontend/job-application-helper-ui/src/app/resume/resume-editor/resume-editor.component.ts
+++ frontend/job-application-helper-ui/src/app/resume/resume-editor/resume-editor.component.ts
@@ -306,7 +306,8 @@ export class ResumeEditorComponent implements CanComponentDeactivate, OnInit, On
 
     ConfirmationDialogComponent.OpenConfirmationDialog(this.dialog, 'Delete Section', `Do you want to delete this section?`, () =>
     {
-      this.resume.profileEntries = this.resume.profileEntries.filter(pe => !this.resume.resumeInfo.sections[index].entriesId.includes(pe.id));
+      if(this.resume.resumeInfo.sections[index].sectionType === ResumeSectionType.ProfileEntry)
+        this.resume.profileEntries = this.resume.profileEntries.filter(pe => !this.resume.resumeInfo.sections[index].entriesId.includes(pe.id));
       this.resume.resumeInfo.sections.splice(index, 1);
       this.hasUnsavedChanges = true;
     });
